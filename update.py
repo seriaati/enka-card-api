818f27eff0152c525ca9fda212fdf6100a8be458
seria <seria.ati@gmail.com>
2024-04-05 08:19:25 +0900

    feat: Auto update namecard data for ENCard

--- update.py
+++ update.py
@@ -2,9 +2,12 @@ import asyncio
 
 from enkacard2 import encbanner2
 
+from ENCard.encard.src.tools.namecard_map import update_data
+
 
 async def update_assets() -> None:
     await encbanner2.upload()
+    await update_data()
 
 
 asyncio.run(update_assets())
