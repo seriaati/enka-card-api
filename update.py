535bccd4ca83788b5eaf0bc1c4c9c5c2b0cf1ba1
seria <61446626+seriaati@users.noreply.github.com>
2024-04-25 22:35:18 +0900

    fix: FIx how update function is called

--- update.py
+++ update.py
@@ -7,7 +7,7 @@ from ENCard.encard.encard import ENCard
 
 async def update_assets() -> None:
     await encbanner2.upload()
-    await ENCard.update_assets()
+    await ENCard().update_assets()
 
 
 asyncio.run(update_assets())
