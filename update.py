dc2a95187126ff479c9a9ffe52e1bcd09bdb66bc
seria <seria.ati@gmail.com>
2024-05-23 19:33:06 +0900

    Update update script

--- update.py
+++ update.py
@@ -1,11 +1,13 @@
 import asyncio
 
 from enkanetwork import EnkaNetworkAPI
+from starrailcard.src.api import enka
 
 
 async def update_assets() -> None:
     async with EnkaNetworkAPI() as client:
         await client.update_assets()
+    await enka.ApiEnkaNetwork().update_assets()
 
 
 asyncio.run(update_assets())
